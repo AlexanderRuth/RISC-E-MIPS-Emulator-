cb173982b4c12c728ec62029c9d27c044f92cc65
Alex Ruth <alexruth@comcast.net>
2020-09-06 13:09:47 -0400

    Clean up readme

---
+++ build/CMakeFiles/MIPSEmulator.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/MIPSEmulator/src/Driver.cpp" "/home/alex/MIPSEmulator/build/CMakeFiles/MIPSEmulator.dir/src/Driver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/alex/MIPSEmulator/build/src/CMakeFiles/mips_cpu.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/mips_cpu.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/MIPSEmulator/src/CPU.cpp" "/home/alex/MIPSEmulator/build/src/CMakeFiles/mips_cpu.dir/CPU.cpp.o"
+  "/home/alex/MIPSEmulator/src/CPU_Arithmetic.cpp" "/home/alex/MIPSEmulator/build/src/CMakeFiles/mips_cpu.dir/CPU_Arithmetic.cpp.o"
+  "/home/alex/MIPSEmulator/src/CPU_Control.cpp" "/home/alex/MIPSEmulator/build/src/CMakeFiles/mips_cpu.dir/CPU_Control.cpp.o"
+  "/home/alex/MIPSEmulator/src/CPU_Logical.cpp" "/home/alex/MIPSEmulator/build/src/CMakeFiles/mips_cpu.dir/CPU_Logical.cpp.o"
+  "/home/alex/MIPSEmulator/src/CPU_Memory.cpp" "/home/alex/MIPSEmulator/build/src/CMakeFiles/mips_cpu.dir/CPU_Memory.cpp.o"
+  "/home/alex/MIPSEmulator/src/CPU_Parsing.cpp" "/home/alex/MIPSEmulator/build/src/CMakeFiles/mips_cpu.dir/CPU_Parsing.cpp.o"
+  "/home/alex/MIPSEmulator/src/CPU_Utilities.cpp" "/home/alex/MIPSEmulator/build/src/CMakeFiles/mips_cpu.dir/CPU_Utilities.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
